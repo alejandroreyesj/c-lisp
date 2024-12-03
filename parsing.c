8a4fa265aba55cd0d95e10b06a33ccedfdd3c9a7
Alejandro Reyes <36049414+alejandroreyesj@users.noreply.github.com>
2024-12-02 20:03:25 -0500

    Create First Parser for Polish Notation

--- parsing.c
+++ parsing.c
@@ -1,3 +1,4 @@
+#include "mpc.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,12 +26,23 @@ void add_history(char *unused) {}
 
 int main(int argc, char **argv) {
 
-  puts("Welcome!");
+  mpc_parser_t *Number = mpc_new("number");
+  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
+  mpc_parser_t *Lispy = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT, "                                               \
+      number: /-?[0-9]+/;                           \
+      operator: '+' | '-' | '*' | '/';              \
+      expr: <number> | '(' | <operator> <expr>+ ')';\
+      lispy: /^/ <operator> <expr>+ /$/;            \
+    ",
+            Number, Operator, Expr, Lispy);
+
   puts("Lispy Version 0.0.0.0.2");
   puts("Press CTRL+c to Exit");
 
   while (true) {
-
     char *input = readline("c-lisp> ");
     add_history(input);
 
